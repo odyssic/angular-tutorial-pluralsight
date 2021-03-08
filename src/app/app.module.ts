d3bc5748459baa76ceb57e5b8ffb17781d1a33df
odyssic <stevenbuddenjr@gmail.com>
2021-03-08 13:15:35 -0500

    input passing event from list to thumbnail

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { EventsAppComponent } from "./events-app.component";
+
+import { EventListComponent } from "./events/events-list.component";
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventThumbnailComponent,
+    EventListComponent,
+  ],
+  imports: [BrowserModule],
+  bootstrap: [EventsAppComponent],
+})
+export class AppModule {}
