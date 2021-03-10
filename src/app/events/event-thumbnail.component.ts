ecff17c972a3b2c2767e32eae61e75adef308f29
odyssic <stevenbuddenjr@gmail.com>
2021-03-10 10:40:55 -0500

    applying initial css styles

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -12,21 +12,26 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
         <div>Price:\${{ event.price }}</div>
         <div>
           <span>Location: {{ event.location.address }}</span>
-          <span>&nbsp;</span>
-          <span>{{ event.location.city }}, {{ event.location.country }}</span>
+
+          <span class="pad-left"
+            >{{ event.location.city }}, {{ event.location.country }}</span
+          >
         </div>
-        <button class="btn btn-primary" (click)="handleClickMe()">
-          Click Me
-        </button>
       </div>
     </div>
   `,
+
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `,
+  ],
 })
 export class EventThumbnailComponent {
   @Input() event: any;
-  @Output() eventClick = new EventEmitter();
-
-  handleClickMe() {
-    this.eventClick.emit("foo");
-  }
 }
